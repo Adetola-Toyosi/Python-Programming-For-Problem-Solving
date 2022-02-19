836679fd0441cc129d4f6cca5482df92dceee811
Adetola-Toyosi <5adeto69@solent.ac.uk>
2022-02-19 17:49:40 +0000

    Created program to write to a text file

---
+++ basics/functions/function_with_loop.py
@@ -0,0 +1,6 @@
+def cross_bridge(distance):
+    return "Crossed step." * distance
+    if distance > 5:
+        return "The bridge is collapsing"
+    else:
+        return ""
---
+++ data/text/write/write_line.py
@@ -0,0 +1,14 @@
+file_path = input("Enter a file path:\n")
+default_file_path = "output.txt"
+
+try:
+    if file_path == "":
+        with open(default_file_path, "w") as file1:
+            file1.write("Data has been overwritten!")
+    else:
+        with open(file_path, "w") as file2:
+            file2.write("Data has been overwritten!")
+    print("The data has been written into the file")
+
+except IOError:
+    print("Please do the right thing.")
