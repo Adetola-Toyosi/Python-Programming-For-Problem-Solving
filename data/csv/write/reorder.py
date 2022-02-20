1afac067e0d194ef5b852d566952043302971e70
Adetola-Toyosi <5adeto69@solent.ac.uk>
2022-02-20 21:40:15 +0000

    Created a program to read and export data to a CSV file.

---
+++ data/csv/write/reorder.py
@@ -0,0 +1,13 @@
+import csv
+
+file_path = "../songs.csv"
+dest_file_path = "output.csv"
+
+with open(file_path) as file1:
+    with open(dest_file_path, 'w') as file2:
+        csv_reader = csv.reader(file1)
+        headings = next(csv_reader)
+        file2.write(str(headings))
+
+        for values in csv_reader:
+            file2.write(f"\n{str(values)}")
