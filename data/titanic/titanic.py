b80bf50f479d8d11711b5e4d35d9e0d1c5e4b78e
Adetola-Toyosi <5adeto69@solent.ac.uk>
2022-02-26 13:45:49 +0000

    Displayed an options menu for the titanic case study.

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -3,7 +3,7 @@ import csv
 file_path = "titanic.csv"
 records = []
 
-print("Loading data...", end="")
+print("Loading data...", end="")  #the 'end' parameter means the next print statement would not be on a new line
 
 with open(file_path, "r") as file:
     csv_reader = csv.reader(file)
@@ -14,3 +14,13 @@ with open(file_path, "r") as file:
         records.append(values)
     print("Done!")
     print(f"Successfully loaded {len(records)} records")
+
+option = int(input(
+"""
+Please select one of the following options:
+[1] Display names of all passengers
+[2] Display the number of passengers that survived
+[3] Display the number of passengers per gender
+[4] Display the number of passenger per age group
+\n"""))
+print(f"You have selected option: {option}")
