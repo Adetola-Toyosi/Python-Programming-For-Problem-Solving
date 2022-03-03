eac151a4401150560780664f11db24b5aa692597
Adetola-Toyosi <5adeto69@solent.ac.uk>
2022-03-03 22:09:24 +0000

    Added code to demonstrate a simple dictionary.

---
+++ data/dicts/simple_dict.py
@@ -0,0 +1,9 @@
+def pattern():
+    sequences = {}
+    sequences["Short Sequence"] = 3
+    sequences["Medium Sequence"] = 2
+    sequences["Long Sequence"] = 1
+
+    return sequences
+
+# print(pattern())
