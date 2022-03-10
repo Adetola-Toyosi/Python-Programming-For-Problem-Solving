ace8447eff0fc133f5a29ed86ba3ee2d7a68acf2
Oluwatoyosi Adeola Adetola <5adeto69@solent.ac.uk>
2022-03-10 10:51:46 +0000

    Created a progra m to retrieve a single record from a database

--- persistence/sqlite/select_one.py
+++ persistence/sqlite/select_one.py
@@ -3,3 +3,9 @@ import sqlite3
 db = sqlite3.connect('users.db')
 cursor = db.cursor()
 
+cursor.execute("SELECT * FROM users")
+
+single_record = cursor.fetchone()
+print(single_record)
+
+db.close()
