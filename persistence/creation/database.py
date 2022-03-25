d97ca4340fc367335709a9f92bfdbaa4ad163636
Oluwatoyosi Adeola Adetola <5adeto69@solent.ac.uk>
2022-03-25 13:52:22 +0000

    Created a function to display a list of presenters with their organisations

---
+++ persistence/creation/database.py
@@ -0,0 +1,14 @@
+import main
+
+print("Please select one of the following options: ")
+
+entry = int(input("""
+    [1] Display a list of all presenters with their organisation
+    [2] Display a list of events with their locations
+    [3] Display a list of presenters for a specified event
+    [4] Display a list of all events for a specific presenter \n
+"""))
+print()
+
+if entry == 1:
+    main.presenters_with_organisation()
--- persistence/creation/main.py
+++ persistence/creation/main.py
@@ -1,2 +1,19 @@
 import sqlite3 as sql
 
+db = sql.connect('events.db')
+cursor = db.cursor()
+
+def presenters_with_organisation():
+    """To display a list of presenters with their organisations"""
+    cursor.execute("""
+    SELECT * FROM presenter p
+    INNER JOIN organisation o
+    ON p.org_id = o.id
+    """)
+
+    records = cursor.fetchall()
+    print("List of presenters with their organisations: ")
+
+    for record in records:
+        print(f"Presenter: {record[1]}, Organisation: {record[4]}")
+
